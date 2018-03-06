31aef7c4a08ae471297ef201045c8f873a7f5f23
Alexander Morozov <alexsvar@icloud.com>
2018-03-06 16:55:52 +0300

    Added Task 3

---
+++ lesson_04/Task 3/validator.js
@@ -0,0 +1,27 @@
+var sendButton = document.querySelector('#sendButton'),
+	formValidation = {
+		'name': /^[A-Za-zА-Яа-я]+$|^[A-Za-zА-Яа-я]+\s[A-Za-zА-Яа-я]+$/i,
+		'phone': /^\+7\(\d{3}\)\d{3}-\d{4}$/,
+		'email': /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/i,
+		'city': /^[A-Za-z/s-]+$/i,
+		'message': /^.+|^(.+\s)+/i
+	};
+
+sendButton.addEventListener('click', function(event) {
+
+	var fields = document.querySelectorAll('input, textarea');
+	for (var i = 0; i < fields.length; i++) {
+
+		if (formValidation[fields[i].name].test(fields[i].value) === true) {
+			fields[i].classList.remove('error');
+			fields[i].classList.add('success');
+			fields[i].previousElementSibling.style.display = "none";
+		}
+		else {
+			event.preventDefault();
+			fields[i].classList.add('error');
+			fields[i].classList.remove('success');
+			fields[i].previousElementSibling.style.display = "block";
+		}
+	}
+});
